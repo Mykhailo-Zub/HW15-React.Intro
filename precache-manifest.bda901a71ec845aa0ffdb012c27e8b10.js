eb039dfab1eb2604caf9a519903481f3d59d4dd1
Mykhailo Zub <mikhail.a.zub@gmail.com>
2020-09-06 13:06:38 +0300

    Updates

---
+++ precache-manifest.bda901a71ec845aa0ffdb012c27e8b10.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4541727a23264d1dee5c6682518f769",
+    "url": "/HW15-React.Intro/index.html"
+  },
+  {
+    "revision": "5f9459d9f6909d1c2d3c",
+    "url": "/HW15-React.Intro/static/css/main.0b3dab1c.chunk.css"
+  },
+  {
+    "revision": "da01dc2f6e18290ac7f9",
+    "url": "/HW15-React.Intro/static/js/2.60e1fce5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW15-React.Intro/static/js/2.60e1fce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f9459d9f6909d1c2d3c",
+    "url": "/HW15-React.Intro/static/js/main.e7e74e94.chunk.js"
+  },
+  {
+    "revision": "f3a0b781fef831c4f294",
+    "url": "/HW15-React.Intro/static/js/runtime-main.237498b4.js"
+  },
+  {
+    "revision": "0002e5f6fa980eed02d3897ecc5eb36d",
+    "url": "/HW15-React.Intro/static/media/comment.0002e5f6.svg"
+  },
+  {
+    "revision": "39726703564a2a71e8077948ac5d964c",
+    "url": "/HW15-React.Intro/static/media/confirm.39726703.svg"
+  },
+  {
+    "revision": "da3338122f87907c1fb994f17002f866",
+    "url": "/HW15-React.Intro/static/media/down-arrow.da333812.svg"
+  },
+  {
+    "revision": "12220a2259c8f1718847197c4bfdb84e",
+    "url": "/HW15-React.Intro/static/media/like.12220a22.svg"
+  },
+  {
+    "revision": "444db2463624b56ae55674c6e654d9b9",
+    "url": "/HW15-React.Intro/static/media/share.444db246.svg"
+  },
+  {
+    "revision": "8f047106898ba92d2dc7e03f29f16bed",
+    "url": "/HW15-React.Intro/static/media/upload.8f047106.svg"
+  }
+]);
